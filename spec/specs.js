dc43451a7941ea66709e81543edb2611aec93ba2
Krystan Menne <kmenne@gmail.com>
2015-11-14 18:38:20 -0800

    Create test for pingpong function

--- spec/specs.js
+++ spec/specs.js
@@ -20,6 +20,21 @@ describe('validateInput', function() {
     });
 });
 
+describe('pingPongTest', function() {
+    it("returns pingpong for a number that is divisible by 3 and 5", function() {
+        expect(pingPongTest(15)).to.equal("pingpong");
+    });
+    it("returns ping for a number that is divisible by 3", function() {
+        expect(pingPongTest(3)).to.equal("ping");
+    });
+    it("returns pong for a number that is divisible by 5", function() {
+        expect(pingPongTest(15)).to.equal("pong");
+    });
+    it("returns the number for a number that is divisible by 3 and 5", function() {
+        expect(pingPongTest(2)).to.equal(2);
+    });
+});
+
 describe('generatePingPongs', function() {
     it("generates an array of [1,2] when given 2 as an input", function() {
         expect(generatePingPongs(2)).to.have.same.members([1,2]);
