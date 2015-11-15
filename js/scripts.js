8aa4c29a3179c24ad06efb1b0a8b952f0cfd5f55
Krystan Menne <kmenne@gmail.com>
2015-11-14 17:03:49 -0800

    Modify DOM upon button click

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,15 @@
  * Created by kmenne on 11/14/15.
  */
 var validateInput = function(userNumber) {
-    if (typeof(userNumber) === "number") {
-        return (userNumber >= 1)
-    } else {
-        return false
-    }
+  if (typeof(userNumber) === "number") {
+    return (userNumber >= 1);
+  } else {
+    return false;
+  }
 };
+
+$(document).ready(function() {
+  $("button").click(function() {
+    $("ul#pingPong").append("<li>Hello world!</li>");
+  });
+});
